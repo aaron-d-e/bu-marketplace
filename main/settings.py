e9c4206264ffbb4c64b6d25ebfe71d0549fe5470
Aaron Evans <aarone0621@gmail.com>
2026-02-24 00:30:44 -0600

    gitignore

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t!&h6&0yj@^eeixxe-u7pbzyru--&_v@g(+gbi^n_u4f5m7qj#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with mebug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
