fa81d0324d894b9909e89339a736550c559ed114
Erick Martinez <erickmartinez0131@gmail.com>
2026-02-24 17:04:08 -0600

    admin panel button redirection routing

--- market_app/views.py
+++ market_app/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
 
 
 def home(request):
-    now = datetime.datetime.now()
-    html = '<html lang="en"><body>It is now %s.</body></html>' % now
-    return HttpResponse(html)
+    return render(request, 'market_app/home.html')
 
 
 def redirect(request):
