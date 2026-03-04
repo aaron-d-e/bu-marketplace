6c89a208944f47313d359769a91fb94d8a5f114f
Aaron Evans <aarone0621@gmail.com>
2026-03-04 12:42:26 -0600

    statics

--- main/settings.py
+++ main/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Supabase Storage Configuration (S3-compatible)
